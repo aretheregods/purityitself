533ef9f59a3b557fa6241ba7e9de868b7782c93e
aretheregods <aretheregods@gmail.com>
2016-12-28 11:02:11 -0500

    Added precache code

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,31 @@
 importScripts('/bower_components/sw-toolbox/sw-toolbox.js');
+
+// Version 1.1
+
+toolbox.precache([
+    'index.html',
+    'resume.html',
+    'thoughts.html',
+    'contact.html',
+    'education.html',
+    'work.html',
+    'activism.html',
+    '404.html'
+    ],
+    [
+    'YSupergirl.png',
+    'about_image.png',
+    'education.jpg',
+    'work.jpg',
+    'activist.jpg',
+    'tw_profile.jpg'
+    ],
+    [
+    'styles.css'
+    ],
+    [
+    'sw.js',
+    'service-worker.js'
+])
  
 toolbox.router.default = toolbox.cacheFirst;
